d262ee294eea2b3d9937b17974b3f427459c2c87
0Sheraz <121394436+0Sheraz@users.noreply.github.com>
2023-03-25 15:26:15 -0700

    Quick Changes
    Quick Changes

--- js/Scripts/index.js
+++ js/Scripts/index.js
@@ -1,8 +1,9 @@
+// feauters script not worked 
 const continentSelect = document.getElementById('continent');
 const activitySelect = document.getElementById('activity');
 const submitButton = document.querySelector('.btn');
 
-// Define options for each continent
+ 
 const options = {
   oceania: [ 'skiing'],
   asia: [  'Mountaineering'],
@@ -10,7 +11,7 @@ const options = {
   europe: ['Ice-Climbing']
 };
 
-// Populate the activity options based on the selected continent
+ 
 continentSelect.addEventListener('change', () => {
   const selectedContinent = continentSelect.value;
   const selectedOptions = options[selectedContinent];
@@ -23,10 +24,15 @@ continentSelect.addEventListener('change', () => {
   });
 });
 
-// Handle the form submission
+ 
 submitButton.addEventListener('click', (event) => {
   event.preventDefault();
   const selectedContinent = continentSelect.value;
   const selectedActivity = activitySelect.value;
   console.log(`Take me to ${selectedContinent} for ${selectedActivity}`);
 });
+// feauters script not worked 
+
+// About us event listners and function 
+
+// About us event lisyners and function 

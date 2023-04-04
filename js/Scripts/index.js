96121f7509658379f3fb8ac45bb9384c8711256d
atif2023 <atif2210e@aptechgdn.net>
2023-04-03 20:01:41 -0700

    homepage done
    homepage done

--- js/Scripts/index.js
+++ js/Scripts/index.js
@@ -215,3 +215,22 @@ var swiper = new Swiper('.blog-slider', {
       clickable: true,
     }
   });
+//   end 
+// go to top
+$(document).ready(function() {
+    // Show or hide the sticky footer button
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 200) {
+            $('.go-top').fadeIn(200);
+        } else {
+            $('.go-top').fadeOut(200);
+        }
+    });
+    
+    // Animate the scroll to top
+    $('.go-top').click(function(event) {
+        event.preventDefault();
+        
+        $('html, body').animate({scrollTop: 0}, 300);
+    })
+});
